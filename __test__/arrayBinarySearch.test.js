c7086aeb426816775b8b74c2f6606dcf4fb7bee3
daniaalrababa9 <rababadania@gmail.com>
2020-01-22 00:07:16 +0200

    done

--- __test__/arrayBinarySearch.test.js
+++ __test__/arrayBinarySearch.test.js
@@ -1,7 +1,11 @@
 'use strict';
-const arithmetic=require('../challenges/arrayBinarySearch')
+const arithmetic=require('../challenges/arrayBinarySearch/array-binary-search.js')
 describe('Arithmetic Module',()=>{
-    it('validate it will retern -1 if the number is not in the array',()=>{
-        expect(arithmetic.binarySearch([11,22,33,44,55,66,77], 90)).toEqual(-1);
+    it('validate if the function return -1 if the value isnot in the array',()=>{
+        expect(arithmetic([11,22,33,44,55,66,77], 90)).toEqual(-1)
     })
+    it('validate if the function return the index of the value in the array if the value is in the array',()=>{
+        expect(arithmetic([4,8,15,16,23,42], 15)).toEqual(2)
+    })
+  
 })
--- challenges/arrayBinarySearch/array-binary-search.js
+++ challenges/arrayBinarySearch/array-binary-search.js
@@ -1,13 +1,16 @@
 'use strict';
-const arithmetic=module.exports={}
-arithmetic.binarySearch((arr,val)=>{
-for(let i=0;i<=arr.length;i++){
-    if(arr[i]=== val){
-        output=i
-        break
-    }else{
-        output=-1
+function binaryArraySearch(array, value) {
+    let beginning=0
+    let theEndOfIt=array.length-1
+    while (beginning <= theEndOfIt) {
+        let middle = Math.floor((beginning + theEndOfIt) / 2);
+        if (array[middle] === value) 
+        return middle;
+        else if (array[middle] < value)
+            beginning = middle + 1;
+        else
+            theEndOfIt = middle - 1;
     }
-    return output
+    return -1;
 }
-})
+module.exports=binaryArraySearch
