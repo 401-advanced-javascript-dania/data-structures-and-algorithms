0de60f72b533f3b07a41bdc04803e02992e99292
daniaalrababa9 <rababadania@gmail.com>
2020-01-22 01:09:02 +0200

    done

---
+++ __test__/arrayShift.test.js
@@ -0,0 +1,12 @@
+'use strict';
+const arithmetic=require('../challenges/arrayShift/array-shift.js')
+describe('Arithmetic Module',()=>{
+    it('validate if the function return a new array that added the value to it middle ',()=>{
+        expect(arithmetic([2,4,6,8], 5).length).toEqual(5)
+    })
+    it('validate if the function return a new array that added the value to it middle ',()=>{
+        let array=arithmetic([2,4,6,8], 5)
+        expect(array[2]).toEqual(5)
+    })
+  
+})
