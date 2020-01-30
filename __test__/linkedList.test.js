638af0de95e2d528df1a3b072e7d3bbf0015752c
daniaalrababa9 <rababadania@gmail.com>
2020-01-31 01:24:16 +0200

    add some files

--- __test__/linkedList.test.js
+++ __test__/linkedList.test.js
@@ -6,18 +6,28 @@ describe('LL Module',()=>{
         expect(list.head).toBeNull();
 
     })
-    it('append',()=>{
+    it('append()',()=>{
 let list = new LL();
 let initialValue ='first value';
 list.append(initialValue);
 expect(list.head.value).toEqual(initialValue)
 expect(list.head.value.next).toBeAnObject;
     })
-    it('insertBefore',()=>{
+   
+    it('insertBefore()',()=>{
         let list = new LL();
         let initialValue ='first value';
         list.append(initialValue);
         list.insertBefore(initialValue,'second value')
         expect(list.head.value).toEqual('second value')
     })
+    it('insertAfter()',()=>{
+        let list = new LL();
+        let initialValue ='first value';
+        list.append(initialValue);
+        list.insertAfter(initialValue,'second value')
+        expect(list.head.value).toEqual('first value')
+        expect(list.head.value.next).toBeAnObject;
+    })
+
 })
