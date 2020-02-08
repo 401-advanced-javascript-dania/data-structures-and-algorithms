eb06f91d84b49c3304738792b801ab2f2c0fc3b8
daniaalrababa9 <rababadania@gmail.com>
2020-02-09 01:27:31 +0200

    done

--- __test__/queueWithStacks.test.js
+++ __test__/queueWithStacks.test.js
@@ -1,16 +1,34 @@
-'use strict';
-const Queue = require('../challenges/queueWithStacks/queue-with-stacks.js')
-describe('Queue Class',()=>{
-    it ('Can successfully enqueue into a queue',()=>{
-        let queue = new Queue();
-        queue.enqueue('test value1');
-        expect(queue.stack1).toEqual(['test value1']);
-      })
-      it('Can successfully enqueue multiple values into a queue',()=>{
-        let queue = new Queue();
-        queue.enqueue('test value1');
-        queue.enqueue('test value2');
-        queue.enqueue('test value3');
-        expect(queue.stack1).toEqual(['test value1','test value2','test value3']);
-      });
-})
+
+const PseudoQueue = require('../challenges/queueWithStacks/queue-with-stacks.js');
+describe('PseudoQueue Class',()=>{
+  it ('Can successfully enqueue into a queue',()=>{
+    let queue = new PseudoQueue();
+    queue.enqueue('test value1');
+    expect(queue.stack1).toEqual(['test value1']);
+  });
+  it('Can successfully enqueue multiple values into a queue',()=>{
+    let queue = new PseudoQueue();
+    queue.enqueue('test value1');
+    queue.enqueue('test value2');
+    queue.enqueue('test value3');
+    expect(queue.stack1).toEqual(['test value1','test value2','test value3']);
+  });
+  it ('Can successfully dequeue value',()=>{
+    let queue = new PseudoQueue();
+    queue.enqueue('test value1');
+    queue.enqueue('test value2');
+    queue.enqueue('test value3');
+    queue.dequeue();
+    expect(queue.stack1).toEqual(['test value2','test value3']);
+  });
+  it('Can successfully dequeue multiple values ',()=>{
+    let queue = new PseudoQueue();
+    queue.enqueue('test value1');
+    queue.enqueue('test value2');
+    queue.enqueue('test value3');
+    queue.dequeue();
+    queue.dequeue();
+    queue.dequeue();
+    expect(queue.stack1).toEqual([]);
+  });
+});
--- challenges/queueWithStacks/queue-with-stacks.js
+++ challenges/queueWithStacks/queue-with-stacks.js
@@ -1,26 +1,32 @@
-'use strict';
-class Queue {
-    constructor(){
-        this.stack1 = [];
-        this.stack2 = [];
-        this.top = null;
 
-    }
-    enqueue(value){
-this.stack1.push(value);
-this.top = value;
+class PseudoQueue {
+  constructor(){
+    this.stack1 = [];
+    this.stack2 = [];
+    this.top = null;
 
-    }
-    denqueue(){
-if (this.stack2.length === 0){
-    if(this.stack1.length === 0){
-        return 'there is no value in the queue'
-    }
+  }
+  enqueue(value){
+    this.stack1.push(value);
+    this.top = value;
+
+  }
+  dequeue(){
+    if (this.stack2.length === 0){
+      if(this.stack1.length === 0){
+        return 'there is no value in the queue';
+      }}
     while(this.stack1.length > 0){
-        let value =this.stack1.pop();
-        this.stack2.push(value)
+      let value = this.stack1.pop();
+      this.stack2.push(value);
     }
-}return this.stack2.pop();
+
+    const data = this.stack2.pop();
+    while(this.stack2.length > 0){
+      let value = this.stack2.pop();
+      this.stack1.push(value);
     }
+    return data;
+  }
 }
-module.exports=Queue;
+module.exports = PseudoQueue;
